57f75c40682b83e627b18687ec769abd01dfb25a
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2025-11-03 21:53:55 -0600

    added log messages for model

--- evaluation/train_search.py
+++ evaluation/train_search.py
@@ -103,6 +103,8 @@ def infer(valid_queue, model, criterion, attack_f, args):
     for step, (input, target) in enumerate(valid_queue):
         input  = input.to(args.device, non_blocking=True)
         target = target.to(args.device, non_blocking=True)
+        print("Tipo de model._modules:", type(model._modules))
+        print("Contenido de model._modules:", model._modules)
         attack = attack_f(model)
         adv_input = attack(input, target).to(args.device, non_blocking=True)
 
