5c660c0e42ae25ba6ac9932c46c6074d1198ba27
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2025-11-06 22:38:17 -0600

    changed FGSM code

--- adversarial.py
+++ adversarial.py
@@ -1,6 +1,17 @@
 from fractions import Fraction
-
+import torch
 import torchattacks
+
+def fgsm(model, x, y, eps=8/255):
+    was_training = model.training
+    model.eval()
+    x = x.detach().clone().requires_grad_(True)
+    logits = model(x)
+    loss = torch.nn.functional.cross_entropy(logits, y)
+    loss.backward()
+    adv = (x + eps * x.grad.sign()).clamp(0, 1).detach()
+    model.train(was_training)
+    return adv
 
 
 def get_attack_function(attack_params):
@@ -8,7 +19,7 @@ def get_attack_function(attack_params):
     if 'alpha' in attack_params['params']:
         attack_params['params']['alpha'] = float(Fraction(attack_params['params']['alpha'])) if '/' in attack_params['params']['alpha'] else float(attack_params['params']['alpha'])
     if attack_params['name'] == 'FGSM':
-        attack_function = lambda model: torchattacks.FGSM(model, **attack_params['params'])
+        attack_function = lambda model, x, y, eps: fgsm(model, x, y, eps=attack_params['params']['eps'])
     elif 'PGD' in attack_params['name']:
         attack_function = lambda model: torchattacks.PGD(model, **attack_params['params'])
     else:
