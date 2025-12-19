ed3bd96b940af6e0e7382b8208fee7f95dce3eaf
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2025-12-18 23:03:27 -0600

    fix in archives

--- archivers.py
+++ archivers.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 
-def dominates(ind1, ind2):
-    if np.allclose(ind1.F, ind2.F, atol=1e-8):
+def dominates(ind1, ind2, k):
+    if np.allclose(ind1.F[:k], ind2.F[:k], atol=1e-8):
         return False
-    return all(f1 <= f2 for f1, f2 in zip(ind1.F, ind2.F))
+    return all(f1 <= f2 for f1, f2 in zip(ind1.F[:k], ind2.F[:k]))
 
 
 
 # Return non-dominated points in archive
-def archive_update_pq(archive, population):
+def archive_update_pq(archive, population, k=4):
     for ind in population:
         dominated = False
         to_remove = []
         for i, arch_ind in enumerate(archive):
-            if dominates(arch_ind, ind):
+            if dominates(arch_ind, ind, k):
                 dominated = True
                 break
-            elif dominates(ind, arch_ind):
+            elif dominates(ind, arch_ind, k):
                 to_remove.append(i)
         if not dominated:
             for i in reversed(to_remove):
