2d475eac7f8a77d870573aa4aa9b3a2f9b851637
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2025-12-21 20:58:15 -0600

    updated batch

--- algorithms/nevonas/scripts/s1/arch_search_sync.sh
+++ algorithms/nevonas/scripts/s1/arch_search_sync.sh
@@ -26,11 +26,11 @@ valid_batch_size=1024
 
 if [ $train_discrete -gt 0 ]
 then
-  python3 ./s1/arch_search.py --gpu ${gpu} --init_channel ${init_channel} --layers ${layers} --dataset ${dataset} \
+  CUDA_LAUNCH_BLOCKING=1 PYTHONMALLOC=debug python3 -X dev ./s1/arch_search.py --gpu ${gpu} --init_channel ${init_channel} --layers ${layers} --dataset ${dataset} \
                                  --epochs ${epochs} --train_epochs ${train_epochs} --knn ${knn} \
                                  --mutate_rate ${mutate_rate} --pop_size ${pop_size} --train_discrete
 else
-  python3 ./s1/arch_search.py --gpu ${gpu} --init_channel ${init_channel} --layers ${layers} --dataset ${dataset} \
+  CUDA_LAUNCH_BLOCKING=1 PYTHONMALLOC=debug python3 -X dev ./s1/arch_search.py --gpu ${gpu} --init_channel ${init_channel} --layers ${layers} --dataset ${dataset} \
                                  --epochs ${epochs} --train_epochs ${train_epochs} --knn ${knn} \
                                  --mutate_rate ${mutate_rate} --pop_size ${pop_size}
 fi
