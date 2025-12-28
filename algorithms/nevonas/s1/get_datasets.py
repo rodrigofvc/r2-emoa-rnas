4653917ac309f02e9ee09362370493b855f1c144
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2025-12-27 22:03:44 -0600

    updated dataloader nevonas

--- algorithms/nevonas/s1/get_datasets.py
+++ algorithms/nevonas/s1/get_datasets.py
@@ -38,25 +38,25 @@ def get_dataloader(args):
     train_transform, valid_transform = ut.data_transforms_cifar100(args.cutout, args.cutout_length, args.autoaug)
     train_data = dset.CIFAR100(root=args.data_dir, train=True , transform=train_transform, download=True)
     assert len(train_data) == 50000, "Something wrong with the dataset loading"
-    if args.split_option == 1:
-      with open(f'{args.config_root}/cifar100_80-split.txt', 'r') as f:
-        cifar100_split = json.load(f)
-      cifar100_split = dict2config(cifar100_split, logger=None)
-      assert len(cifar100_split.train)==40000, 'Something wrong with the training data split'
-      assert len(cifar100_split.valid)==10000, 'Something wrong with the validation data split'
-    else:
-      with open(f'{args.config_root}/cifar100-split.txt', 'r') as f:
-        cifar100_split = json.load(f)
-      cifar100_split = dict2config(cifar100_split, logger=None)
-      assert len(cifar100_split.train)==25000, 'Something wrong with the training data split'
-      assert len(cifar100_split.valid)==25000, 'Something wrong with the validation data split'
-    train_queue = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size,
-                                               pin_memory = False, num_workers = args.workers,
-                                               sampler = torch.utils.data.sampler.SubsetRandomSampler(cifar100_split.train)
-                                              )
-    valid_queue = torch.utils.data.DataLoader(train_data, batch_size=args.valid_batch_size,
-                                               pin_memory = False, num_workers = args.workers,
-                                               sampler = torch.utils.data.sampler.SubsetRandomSampler(cifar100_split.valid)
-                                              )
+
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(args.train_portion * num_train))
+
+    if torch.backends.mps.is_available():
+        # testing
+        split = 96
+        num_train = split + 96
+    print(f"Training samples: {split}, Validation samples: {num_train - split}")
+
+    train_queue = torch.utils.data.DataLoader(
+      train_data, batch_size=args.batch_size,
+      sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[:split]),
+        num_workers=0, pin_memory=False, drop_last=True)
+
+    valid_queue = torch.utils.data.DataLoader(
+      train_data, batch_size=args.batch_size,
+      sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]),
+        num_workers=0, pin_memory=False, drop_last=True)
 
   return train_transform, valid_transform, train_queue, valid_queue
