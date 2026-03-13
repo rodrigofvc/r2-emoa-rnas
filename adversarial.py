84265884a9b2032f7048d4dd9efbe7fa60d114c3
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2026-03-13 12:20:37 -0600

    fix flags

--- adversarial.py
+++ adversarial.py
@@ -1,8 +1,7 @@
 from fractions import Fraction
 import torch
-import torchattacks
+#import torchattacks
 import torch.nn.functional as F
-from torch import amp
 
 
 
@@ -44,11 +43,9 @@ def fgsm_simple(model, x, y, eps=8/255):
     if torch.cuda.is_available():
         torch.cuda.synchronize()
 
-    #with torch.enable_grad():
-    with torch.backends.cudnn.flags(enabled=False):
-        with torch.enable_grad():
-            std_logits = model(x_adv)
-            std_loss = F.cross_entropy(std_logits, y)
+    with torch.enable_grad():
+        std_logits = model(x_adv)
+        std_loss = F.cross_entropy(std_logits, y)
 
     grad = torch.autograd.grad(std_loss, x_adv, only_inputs=True, retain_graph=False, create_graph=False)[0]
     adv = (x_adv + eps * grad.detach().sign()).clamp(0.0, 1.0).detach().clone().contiguous()
