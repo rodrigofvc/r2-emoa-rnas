8b3b1736c789eae9ab2e6bdcb6599d0655cef7c7
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2026-03-13 11:29:40 -0600

    fix flags

--- adversarial.py
+++ adversarial.py
@@ -46,8 +46,9 @@ def fgsm_simple(model, x, y, eps=8/255):
 
     #with torch.enable_grad():
     with torch.backends.cudnn.flags(enabled=False):
-        std_logits = model(x_adv)
-        std_loss = F.cross_entropy(std_logits, y)
+        with torch.enable_grad():
+            std_logits = model(x_adv)
+            std_loss = F.cross_entropy(std_logits, y)
 
     grad = torch.autograd.grad(std_loss, x_adv, only_inputs=True, retain_graph=False, create_graph=False)[0]
     adv = (x_adv + eps * grad.detach().sign()).clamp(0.0, 1.0).detach().clone().contiguous()
