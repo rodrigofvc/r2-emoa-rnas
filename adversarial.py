5c56dfcc4f1df6276ba56385f0b9551b782ee918
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2026-03-24 13:25:43 -0600

    fix

--- adversarial.py
+++ adversarial.py
@@ -15,11 +15,11 @@ def set_attack_mode(model, training):
             m.__dict__['training'] = training
 
 def fgsm_simple(model, x, y, eps=8/255):
-    x_adv = x.detach().requires_grad_(True)
+    x_adv = x.detach().clone().requires_grad_(True)
     with torch.enable_grad():
         std_logits = model(x_adv)
         std_loss = F.cross_entropy(std_logits, y)
 
     grad = torch.autograd.grad(std_loss, x_adv, retain_graph=False, create_graph=False)[0]
-    adv = (x_adv + eps * grad.detach().sign()).clamp(0.0, 1.0).detach()
+    adv = (x_adv + eps * grad.detach().clone().sign()).clamp(0.0, 1.0).detach().clone()
     return adv
