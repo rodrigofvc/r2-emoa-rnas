46ecbca0764cae23a878a1e64535407e7d9aa37e
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2026-06-23 18:06:58 -0600

    fix

---
+++ algorithms/nsga-net/search/individual.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class Individual:
+    def __init__(self, X, k, search_space):
+        assert type(X) == np.ndarray
+        if search_space == 'continuous':
+            self.X = X.astype(np.float32).copy()
+        else:
+            self.X = X.astype(np.int32).copy()
+        self.F = None
+        self.k = k
+        self.std_acc = 0.0
+        self.adv_acc = 0.0
+        self.genotype = None
+        self.feasible = False
+
+    def to_dict(self):
+        return {
+            'X': self.X.tolist(),
+            'F': self.F.tolist(),
+            'k': self.k,
+            'std_acc': self.std_acc,
+            'adv_acc': self.adv_acc,
+            'genotype': self.genotype,
+            'feasible': self.feasible
+        }
+
+def wrap_individuals(pop_obj, pop_X):
+    individuals = []
+    for i in range(len(pop_obj)):
+        ind = Individual(X=pop_X[i], k=len(pop_obj[i]), search_space='continuous')
+        ind.F = pop_obj[i]
+        individuals.append(ind)
+        if np.isfinite(ind.F).any():
+            ind.feasible = True
+    return individuals
+
+def create_from_json(json_dict, search_space):
+    ind = Individual(X=np.array(json_dict['X']), k=json_dict['k'], search_space=search_space)
+    if json_dict['F'] is not None:
+        ind.F = np.array(json_dict['F'])
+    else:
+        ind.F = None
+    ind.std_acc = json_dict['std_acc']
+    ind.adv_acc = json_dict['adv_acc']
+    ind.genotype = json_dict['genotype']
+    ind.feasible = json_dict['feasible']
+    return ind
