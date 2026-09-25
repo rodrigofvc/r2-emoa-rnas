9c50d3f3d233b9d6855713ae4732690fae880885
Rodrigo Velázquez <rodrigo_velazquez99@ciencias.unam.mx>
2026-09-24 20:10:23 -0600

    updated batch

--- batch-w-one-shot.sh
+++ batch-w-one-shot.sh
@@ -15,7 +15,7 @@ for %%S in (18906049 15798821 65381509 27293207 27522793) do (
         --batch_size 192 ^
         --n_population 40 ^
         --generations 31 ^
-        --epochs_warmup 0 ^
+        --epochs_warmup 100 ^
         --epochs_train_supernet 10 ^
         --prob_cross 0.9 ^
         --prob_mut 0.1 ^
